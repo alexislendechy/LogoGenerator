83b0eef13a45c8bd2a023bb9f2866f4423bc8e62
alexislendechy <135558007+alexislendechy@users.noreply.github.com>
2023-09-10 11:49:19 -0600

    render

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
-// index.js
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 const fs = require('fs');
 const { Triangle, Circle, Square } = require('./lib/shapes');
 
-const promptText = [
+const questions = [
   {
     type: 'input',
     name: 'text',
     message: 'Enter up to three characters for the logo text (leave empty for no text):',
   },
-];
-
-const promptColor = (message) => [
   {
     type: 'input',
-    name: 'color',
-    message,
-    validate: (input) => {
-      if (!input) {
-        return true;
-      }
-      return /^(#[0-9A-Fa-f]{6}|[A-Za-z]+)$/i.test(input) || 'Invalid input';
-    },
+    name: 'textColor',
+    message: 'Enter the text color (e.g., "red" or "#FF0000"):',
+  },
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'Choose a shape for the logo:',
+    choices: ['Triangle', 'Circle', 'Square'],
+  },
+  {
+    type: 'input',
+    name: 'shapeColor',
+    message: 'Enter the shape color:',
   },
 ];
-
-const promptShape = {
-  type: 'list',
-  name: 'shape',
-  message: 'Choose a shape for the logo:',
-  choices: ['Triangle', 'Circle', 'Square'],
-};
 
 (async () => {
   console.log(chalk.bold('Logo Generator'));
 
-  const { text } = await inquirer.prompt(promptText);
-  const { color: textColor } = await inquirer.prompt(promptColor('Enter the text color (e.g., "red" or "#FF0000"):'));
-  const { shape } = await inquirer.prompt(promptShape);
-  const { color: shapeColor } = await inquirer.prompt(promptColor(`Enter the ${shape.toLowerCase()} color:`));
+  const answers = await inquirer.prompt(questions);
+
+  const { text, textColor, shape, shapeColor } = answers;
 
   const selectedShape = new (eval(shape))(shapeColor);
   const svgContent = selectedShape.render();
